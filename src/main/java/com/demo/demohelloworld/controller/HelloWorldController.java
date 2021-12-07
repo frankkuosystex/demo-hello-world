82d150981d6874080330d4ddefaec439a212fbb0
2100009 <2100009@21000091frank.systex.tw>
2021-12-07 16:21:44 +0800

    init commit

---
+++ src/main/java/com/demo/demohelloworld/controller/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.demo.demohelloworld.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello World in Spring Boot!";
+    }
+	
+	@RequestMapping("/time")
+    public String showTime() {
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+		Date date = new Date();
+		String strDate = sdFormat.format(date);
+        return "現在時間:"+strDate;
+    }
+	
+	@RequestMapping("/uuid")
+    public String getUuid() {
+		UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+        
+        return uuidAsString;
+    }
+}
---
+++ src/test/java/com/demo/demohelloworld/DemoHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
